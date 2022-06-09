582e77487afc8460e3b0f2be8f6178fc64fac2e9
Justin <jjpeterson90@hotmail.com>
2022-06-09 12:26:24 -0500

    complete

--- python/recursion_challenge.py
+++ python/recursion_challenge.py
@@ -1,11 +1,48 @@
 def factorial(x):
-	pass
+    if x < 2:
+        return 1
+    return x * factorial(x-1)
+
 
 def palindrome(string):
-	pass
+    if len(string) <= 1:
+        return True
+    if string[0] == string[-1]:
+        return palindrome(string[1:-1])
+    return False
 
-def bottles(num):
-	pass
 
-def roman_num(num):
-	pass
+def bottles(num, original=None):
+    if original == None:
+        original = num
+    if num == 0:
+        print('No more bottles of beer on the wall, no more bottles of beer.')
+        if original != 1:
+            print(f'Go to the store and buy some more, {original} bottles of beer on the wall.')
+        else:
+            print(f'Go to the store and buy some more, {original} bottle of beer on the wall.')
+    else:
+        if num == 1:
+            bottle = 'bottle'
+            reduced_num = 'no more'
+            new_bottle = 'bottles'
+        if num >= 2:
+            bottle = 'bottles'
+            reduced_num = num-1
+            if reduced_num == 1:
+                new_bottle = 'bottles'
+            else:
+                new_bottle = 'bottle'
+        print(f'{num} {bottle} of beer on the wall, {num} {bottle} of beer.')
+        print(f'Take one down and pass it around, {reduced_num} {new_bottle} of beer on the wall.')
+        return bottles(num-1, original)
+
+
+
+ROMAN_TO_ARABIC = {'M': 1000,'CM': 900,'D': 500,'CD': 400,'C': 100,'XC': 90,'L': 50,'XL': 40,'X': 10,'IX': 9,'V': 5,'IV': 4,'I': 1,}
+def to_roman_recursive(num, roman_result = '', roman_to_arabic = ROMAN_TO_ARABIC):
+    if num == 0:
+        return roman_result
+    for key in roman_to_arabic:
+        if num / roman_to_arabic[key] >= 1:
+            return to_roman_recursive(num - roman_to_arabic[key], roman_result + key, roman_to_arabic)
--- python/recursion_spec.py
+++ python/recursion_spec.py
@@ -1,1 +1,16 @@
-# Write your unit tests here
+from recursion_challenge import factorial, palindrome, bottles, to_roman_recursive
+
+print("\n\n--== Factorial ==--")
+print(factorial(5)) # 120
+print(factorial(8)) # 40320
+
+print("\n\n--== Palindrome ==--")
+print(palindrome('racecar')) # True
+print(palindrome('railcar')) # False
+
+print("\n\n--== 99 Bottles Song ==--")
+print(bottles(5))
+
+print("\n\n--== Arabic to Roman ==--")
+print(to_roman_recursive(27)) # XXVII
+print("\n")
